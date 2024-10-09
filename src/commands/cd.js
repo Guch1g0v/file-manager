366884bf15618cd52497d128bf27ae4b54c80c43
guchigow <lanov64@yandex.kz>
2024-10-09 20:13:26 +0500

    fix: for unix root directory

--- src/commands/cd.js
+++ src/commands/cd.js
@@ -27,7 +27,9 @@ export const cd = async (currentDir, options) => {
   const [pathToDirectory] = options;
 
   const cleanPath = path.isAbsolute(pathToDirectory)
-    ? pathToDirectory.replace(/[\/\\]$/, '')
+    ? pathToDirectory === '/'
+      ? pathToDirectory
+      : pathToDirectory.replace(/[\/\\]$/, '')
     : path.resolve(currentDir, pathToDirectory);
 
   try {

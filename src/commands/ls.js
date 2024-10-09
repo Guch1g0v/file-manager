3b8a34dcfe18a71035bcef9a7cc57fccaaa4c60e
guchigow <lanov64@yandex.kz>
2024-10-09 20:21:39 +0500

    feat: start ls

--- src/commands/ls.js
+++ src/commands/ls.js
@@ -1,3 +1,26 @@
-export const ls = (currentDir) => {
+import { readdir } from 'node:fs/promises';
+import { ERRORS } from '../constants.js';
+
+const fileType = 'file';
+const dirType = 'directory';
+
+export const ls = async (currentDir) => {
+  const result = [];
+
+  try {
+    const files = await readdir(currentDir, { withFileTypes: true });
+    for (const file of files) {
+      result.push({ Name: file.name, Type: file.isDirectory() ? dirType : fileType });
+    }
+    result.sort((a, b) => {
+      if (a.Type === dirType && b.Type !== dirType) {
+        return -1;
+      }
+      return 1;
+    });
+    console.table(result);
+  } catch {
+    console.error(ERRORS.failed);
+  }
   return currentDir;
 };

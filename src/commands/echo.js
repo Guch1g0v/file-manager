4e1f04617f961cfece1ef6b7e16137e14754aab4
guchigow <lanov64@yandex.kz>
2024-10-10 00:35:19 +0500

    refactor: add JSDoc for echo

--- src/commands/echo.js
+++ src/commands/echo.js
@@ -1,4 +1,10 @@
-export const echo = (currentDir, rest) => {
-  console.log(rest.join(' '));
+/**
+ * Prints the provided options as a concatenated string and returns the current directory.
+ * @param {string} currentDir - The current working directory.
+ * @param {string[]} options - An array of strings to be concatenated and printed.
+ * @returns {string}
+ */
+export const echo = (currentDir, options) => {
+  console.log(options.join(' '));
   return currentDir;
 };
